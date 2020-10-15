b06b618aaac77fe44432c9eeeaf00ecef283d832
LuizFerK <luizfernandoklein@live.com>
2020-10-15 01:34:27 -0300

    feat: Client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface Product {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    })
+  }, []);
+
   return (
     <div>
-      <Title>Hello World</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(product => (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   )
 }
